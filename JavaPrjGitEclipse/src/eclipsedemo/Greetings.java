0f384e3ec535a9d1e20d8767ad0536d08e4705ab
shanemcdaid96 <L00117529@student.lyit.ie>
2018-11-10 14:13:28 +0000

    Committed Greetings.java

---
+++ JavaPrjGitEclipse/src/eclipsedemo/Greetings.java
@@ -0,0 +1,11 @@
+package eclipsedemo;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greetings from Eclipse IDE Ubuntu");
+
+	}
+
+}
