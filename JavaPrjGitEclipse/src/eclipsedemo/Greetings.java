46166e3322fb3f4ad79aa45197c9ea8dbe32b09f
shanemcdaid96 <L00117529@student.lyit.ie>
2018-11-12 08:46:23 +0000

    Made changes to Greetings.java

--- JavaPrjGitEclipse/src/eclipsedemo/Greetings.java
+++ JavaPrjGitEclipse/src/eclipsedemo/Greetings.java
@@ -5,6 +5,7 @@ public class Greetings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Greetings from Eclipse IDE Ubuntu");
+		System.out.println("Current Month is November");
 
 	}
 
